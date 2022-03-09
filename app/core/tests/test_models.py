e316893ce9ba85fa9552ca33e3dddc707af86aec
shahramzs <shahram.zarie.sarabi@gmail.com>
2022-03-09 18:51:24 +0330

    Added custom user model.

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,34 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with an email is successful"""
+        email = "shahram.zarie.sarabi@gmail.com"
+        password = "123456789"
+        user = get_user_model().objects.create_user(email=email,
+                                                    password=password)
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = "shahram.zarie.sarabi@GMAIL.COM"
+        user = get_user_model().objects.create_user(email, 'test123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            "shahram.zarie.sarabi@gmail.com",
+            "13651986")
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
